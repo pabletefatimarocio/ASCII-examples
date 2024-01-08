f84ddbc5838dbdec096970ff164e6c7a89225fa7
pabletefatimarocio <pabletefatima9@gmail.com>
2024-01-08 03:41:01 -0300

    .

---
+++ ascii.js
@@ -0,0 +1,4 @@
+const art = require('ascii-art');
+
+console.log(art.style("Hello World", "red"));
+console.log(art.style('', 'white'));
---
+++ index.js
@@ -0,0 +1,33 @@
+const figlet = require('figlet');
+
+/*figlet('Hello World', (err, result) => {
+    console.log(err || result)
+})*/
+
+figlet('Hola', {
+    font:'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout:'default',
+
+}, (err, result) => {
+  console.log(err || result);
+})
+
+/*figlet.text(
+    "Boo!",
+    {
+      font: "Ghost",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 100,
+      whitespaceBreak: true,
+    },
+    function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+    }
+  );*/
